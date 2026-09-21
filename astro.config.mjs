741c1567b0a077619c873367a90f71877ee5bf47
kahlil13 <73813949+kahlil13@users.noreply.github.com>
2026-09-21 04:48:57 +0000

    Configure native Astro config and GitHub Pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://kahlil13.github.io',
+  base: '/hip-hop-continuum',
+  integrations: [tailwind()]
 });
